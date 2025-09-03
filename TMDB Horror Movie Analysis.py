5a1573bc3a50f434befde884c20c80a04f88fed6
Josh Garzaniti <jgarzani@alumni.nd.edu>
2025-09-03 17:18:01 -0400

    Add files via upload

--- TMDB Horror Movie Analysis.py
+++ TMDB Horror Movie Analysis.py
@@ -14,6 +14,8 @@ import tmdbsimple as tmdb #use either this or tmdbv3api
 import time
 import os
 from dfply import *
+import re
+import matplotlib.pyplot as plt
 
 horror_movies = pd.read_csv("G:\My Drive\Personal Projects\horror_movies.csv")
 
@@ -38,10 +40,45 @@ horror_movies['tagline'] = horror_movies['tagline'].astype(str)
 
 horror_movies['tagline'].apply(type).head()
 
+horror_movies['countries'] = horror_movies['countries'].str.extract(r"\['(.+?)'\]")
 
+horror_movies['countries'].head(5)
 
+horror_movies['production_companies'] = (
+    horror_movies['production_companies']
+    .str.extract(r"\[(.*)\]")[0]
+    .str.replace("'", "", regex=False)  
+    .str.split(", "))
 
+horror_movies['production_companies'].head(5)
 
+horror_movies['release_date'] = pd.to_datetime(horror_movies['release_date'], errors='coerce')
+
+horror_movies['year'] = horror_movies['release_date'].dt.year
+
+horror_ratings_by_year = horror_movies.groupby('year')['vote_average'].mean()
+
+##Graphing out Average Horror Movie TMDB Ratings since 1970
+
+plt.figure(figsize=(12,6))
+plt.plot(horror_ratings_by_year.index, horror_ratings_by_year.values)
+plt.title('Average Horror Rating over Time')
+plt.xlabel('Year')
+plt.ylabel('Average Rating')
+plt.show()
+
+##Graphing out Average Horror Movie Budgets by year
+
+horror_with_budget = horror_movies[horror_movies['budget'] > 0]
+
+horror_budgets_by_year = horror_with_budget.groupby('year')['budget'].mean()
+
+plt.figure(figsize=(12,6))
+plt.plot(horror_budgets_by_year.index, horror_budgets_by_year.values)
+plt.title('Average Horror Budget over Time')
+plt.xlabel('Year')
+plt.ylabel('Average Production Budget')
+plt.show()
 
 
 
